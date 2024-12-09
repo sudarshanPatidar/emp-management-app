0e5e011c8291f1c5faf62fcbb0167194212ad5d6
Sudarshan Patidar <patidarsudarshan2626@gmail.com>
2024-12-10 00:14:54 +0530

    Front end modifications

--- front-end/src/components/designations/ListDesignationsComponent.jsx
+++ front-end/src/components/designations/ListDesignationsComponent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import {useNavigate} from 'react-router-dom'
 import { deleteDesignationApi, retrieveAllDesignations } from "./api/DesignationApiService"
 // import { useAuth } from "./security/AuthContext"
-
+import './ListDesignationsComponent.css'
 function ListDesignationsComponent() {
 
 
